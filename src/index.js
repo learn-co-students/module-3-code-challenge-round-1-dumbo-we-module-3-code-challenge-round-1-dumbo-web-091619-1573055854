68747b523af72edf95d186a648c8002bbe57f35c
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-11-06 12:27:06 -0500

    3rd draft

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 //`http://localhost:3000/beers`
 //<ul class="list-group" id="list-group">
 //<div id="beer-detail">
+//<div id="beer-detail">
 let beerUL = document.getElementById("list-group")
-
+let beerDIV = document.getElementById("beer-detail")
 
 
 
@@ -13,12 +14,12 @@ function loadPage(){
 
 loadPage().then((jsonObj) => {
     jsonObj.forEach((beer) => {
-        console.log(beer)
-     showOneBeer(beer)
+        // console.log(beer)
+     makeList(beer)
 })})
 
 
-function showOneBeer(beer){
+function makeList(beer){
     //<li class="list-group-item">Beer title 1</li>
     let beerLI = document.createElement('li')
     beerLI.className = "list-group-item"
@@ -26,8 +27,47 @@ function showOneBeer(beer){
     beerLI.id = beer.id
     beerUL.append(beerLI)
     beerLI.addEventListener("click", (event) => {
-        console.log(event.target)
-        console.log(beer.name)
+        // console.log(event.target)
+        // console.log(beer.name)
+        fetchOneBeer(event)
     })
     
   }
+
+  function fetchOneBeer(evernt){
+      fetch(`http://localhost:3000/beers/${event.target.id}`)
+      .then(r => r.json())
+      .then((beer) => {
+          showOneBeer(beer)
+      })
+  }
+
+  function showOneBeer(beer){
+    beerDIV.removeChildre
+    let h1 = document.createElement('h1')
+    h1.innerText = beer.name
+    let img = document.createElement('img')
+    img.src= beer.image_url
+    let h3 = document.createElement('h3')
+    h3.innerText = beer.tagline
+    let textarea = document.createElement('textarea')
+    textarea.innerText = beer.description
+    textarea.id = textarea
+    let button = document.createElement('button')
+      button.id = "edit-beer"
+      button.className = "btn btn-info"
+      button.innerText = "Save" 
+      beerDIV.append(h1, img, h3, textarea, button)
+     beerDIV.addEventListener("submit", (e) => {
+         e.preventDefault()
+         console.log(e)
+     })
+
+
+    }
+   
+// beer.tagline}</h3>
+// <textarea>${beer.description}</textarea>
+    
+     
+  
